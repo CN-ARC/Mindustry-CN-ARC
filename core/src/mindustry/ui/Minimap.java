9327405178cfc60dc10a30202f8f7721860bc74d
315923876 <115673942+315923876@users.noreply.github.com>
2025-04-12 10:38:06 +0800

    Update Minimap.java

--- core/src/mindustry/ui/Minimap.java
+++ core/src/mindustry/ui/Minimap.java
@@ -164,10 +164,12 @@ public class Minimap extends Table{
                         var region = renderer.minimap.getRegion();
                         if(region == null) return;
                         
-                        float sx = (cx - x) / width,
-                              sy = (cy - y) / height,
-                              scaledX = Mathf.lerp(region.u, region.u2, sx) * world.width() * tilesize,
-                              scaledY = Mathf.lerp(1f - region.v2, 1f - region.v, sy) * world.height() * tilesize;
+                        float
+                        sx = (cx - x) / width,
+                        sy = (cy - y) / height,
+                        scaledX = Mathf.lerp(region.u, region.u2, sx) * world.width() * tilesize,
+                        scaledY = Mathf.lerp(1f - region.v2, 1f - region.v, sy) * world.height() * tilesize;
+
                         control.input.panCamera(Tmp.v1.set(scaledX, scaledY));
                     }
                 });
