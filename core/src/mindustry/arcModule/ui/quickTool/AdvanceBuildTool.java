87741f346828ea22e5e895003c82b17b72f5e09e
Lucky Clover <jmj18795967847@gmail.com>
2026-05-27 18:14:00 +0800

    解除联机复制限制

--- core/src/mindustry/arcModule/ui/quickTool/AdvanceBuildTool.java
+++ core/src/mindustry/arcModule/ui/quickTool/AdvanceBuildTool.java
@@ -175,7 +175,6 @@ public class AdvanceBuildTool extends ElementUtils.ToolTable {
                 }).size(30).tooltip("地图世处信息");
             }).fillX().row();
             t.table(tt -> tt.button("\uE817", textStyle, () -> shadowBuild = !shadowBuild).checked(a -> shadowBuild).size(30, 30).tooltip("虚影建造模式\n[red]有些服限制发包数较低，建筑较多时会被踢出。请酌情使用")).fillX().row();
-            if (!net.client()) {
                 t.table(tt -> {
                     tt.button("\uF8C9", textStyle, () -> {
                         buildPlansConstrain = !buildPlansConstrain;
@@ -198,7 +197,7 @@ public class AdvanceBuildTool extends ElementUtils.ToolTable {
                     }).size(30, 30).tooltip("复制所选范围内的修饰作为蓝图");
 
                 }).fillX().row();
-            }
+
         });
     }
 
