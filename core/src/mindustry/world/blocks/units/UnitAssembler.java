f3649925669f2fec55d3cd11b3e2c3b407074c3b
Lucky Clover <jmj18795967847@gmail.com>
2026-06-13 13:38:00 +0800

    修复组装厂切换液体无法输入

--- core/src/mindustry/world/blocks/units/UnitAssembler.java
+++ core/src/mindustry/world/blocks/units/UnitAssembler.java
@@ -169,6 +169,7 @@ public class UnitAssembler extends PayloadBlock{
     public void afterPatch(){
         initCapacities();
         super.afterPatch();
+        setLiquidFilter();
     }
 
     public void initCapacities(){
@@ -183,7 +184,11 @@ public class UnitAssembler extends PayloadBlock{
                     itemCapacity = Math.max(itemCapacity, stack.amount * 2);
                 }
             }
+        }
+    }
 
+    public void setLiquidFilter(){
+        for(AssemblerUnitPlan plan : plans){
             if(plan.liquidReq != null){
                 for(LiquidStack stack : plan.liquidReq){
                     liquidFilter[stack.liquid.id] = true;
